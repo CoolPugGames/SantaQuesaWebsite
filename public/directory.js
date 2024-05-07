9272dd53b13b12fa0b064d75e95fb52f5f7f8dd1
GlennGorgoth <arnoldfilm@gmail.com>
2024-05-06 18:43:08 -0600

    changed pictures

---
+++ public/directory.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const containers = document.querySelectorAll('.directory-div');
+    containers.forEach(container => {
+    const grids = container.querySelectorAll('.directory-category');
+    console.log('Adjusting Grid Column Width');
+
+    grids.forEach(grid => {
+        const items = grid.querySelectorAll('.employee-card');
+        const gridItemWidth = grid.querySelector('.employee-card').offsetWidth;
+        const gridContainerWidth = grid.offsetWidth;
+        const numColumns = Math.floor(gridContainerWidth / gridItemWidth);
+
+        // Calculate the padding based on the number of columns
+        const padding = numColumns > 1 ? (numColumns - 1) * 20 : 0;
+
+        // Set the CSS variable for padding
+        gridContainer.style.setProperty('--grid-padding', `0 ${padding}px`);
+        });
+        console.log('Column padding: ',padding);
+
+        // if (items.length <= 5) {
+        //     grid.style.gridTemplateColumns = `repeat(${items.length}, minmax(200px, 1fr))`;
+        // }
+    })
+})
